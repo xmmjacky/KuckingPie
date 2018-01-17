251db7b765584dfe6c08cf44cb29668d801c42c8
xinmi <xinmi@JACKY>
2018-01-17 22:33:51 +0800

    112

---
+++ ADO/Model/Pay/dt_user_confirm_top.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BookingFood.Model.Pay
+{
+   public class dt_user_confirm_top
+    {
+        public int Id { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public DateTime? CreateTime { get; set; }
+
+        public string OpneId { get; set; }
+
+        public int? IsDeleted { get; set; }
+
+        public int? AreaId { get; set; }
+
+        public string AreaName { get; set; }
+    }
+}
---
+++ ADO/Model/Pay/dt_user_top_up.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BookingFood.Model.Pay
+{
+   public class dt_user_top_up
+    {
+        public int Id { get; set; }
+
+        public string OpenId { get; set; }
+
+        public decimal? Amount { get; set; }
+
+        public int? AreaId { get; set; }
+
+        public string AreaName { get; set; }
+
+        public DateTime? CreateTime { get; set; }
+
+        public int? IsDeleted { get; set; }
+
+        public int? Stage { get; set; }
+    }
+}
