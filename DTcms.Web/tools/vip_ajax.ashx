ccc2b44564ba6b402b0dc4ace2ebab3b222c74de
mingming xin <xinmingming0629@163.com>
2018-01-18 18:53:36 +0800

    qwqw

---
+++ DTcms.Web/tools/vip_ajax.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="vip_ajax.ashx.cs" Class="DTcms.Web.tools.vip_ajax" %>
---
+++ DTcms.Web/tools/vip_ajax.ashx.cs
@@ -0,0 +1,31 @@
+﻿using DTcms.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.SessionState;
+
+namespace DTcms.Web.tools
+{
+    /// <summary>
+    /// vip_ajax 的摘要说明
+    /// </summary>
+    public class vip_ajax : IHttpHandler, IRequiresSessionState
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            context.Response.Write("Hello World");
+            string action = DTRequest.GetQueryString("action");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
