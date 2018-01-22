2a23485952cb643301383c912152af871eff251f
mingming xin <xinmingming0629@163.com>
2018-01-22 19:32:26 +0800

    1212

--- DTcms.Web/tools/vip_ajax.ashx.cs
+++ DTcms.Web/tools/vip_ajax.ashx.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.SessionState;
+using Orm.Son.Core;
+using BookingFood.Model.Pay;
 
 namespace DTcms.Web.tools
 {
@@ -12,14 +14,39 @@ namespace DTcms.Web.tools
     /// </summary>
     public class vip_ajax : IHttpHandler, IRequiresSessionState
     {
+        private readonly SonConnection db;
+
+        public vip_ajax()
+        {
+            db = new SonConnection();
+        }
 
         public void ProcessRequest(HttpContext context)
         {
             context.Response.ContentType = "text/plain";
             context.Response.Write("Hello World");
             string action = DTRequest.GetQueryString("action");
+            switch (action)
+            {
+                case "vip_user_charge_list":
+                    ChargeList();
+                    break;
+                default:
+                    break;
+
+            }
         }
 
+        private void ChargeList()
+        {
+
+            var res1 = db.FindMany<dt_user_top_up>(t => t.IsDeleted == 0);
+            var sqlstr = string.Format(@"SELECT w2.n, w1.* FROM dt_user_Top_up w1,
+(SELECT TOP 100 row_number() OVER (ORDER BY CreateTime DESC, ID DESC) n, Id FROM dt_user_Top_up) w2 
+WHERE w1.Id = w2.ID AND w2.n > 1000 ORDER BY w2.n ASC; ");
+            var res = db.ExecuteSql(sqlstr);
+
+        }
         public bool IsReusable
         {
             get
